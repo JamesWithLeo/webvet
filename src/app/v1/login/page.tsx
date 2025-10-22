67b206c5b64f28927e635d73365efdd54ce89fcf
JamesWithLeo <ocampojamesleo04@gmail.com>
2025-10-23 05:10:04 +0800

    responsive

--- src/app/v1/login/page.tsx
+++ src/app/v1/login/page.tsx
@@ -1,12 +1,12 @@
 import GoogleButton from "@/components/GoogleButton";
 import Logo from "@/components/Logo";
-import { Button, Divider, Input, TextInput } from "@mantine/core";
+import { Button, TextInput } from "@mantine/core";
 import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <div className="items-center  grid grid-cols-[1fr]  bg-[url('/pattern.svg')] grid-rows-1 min-h-dvh md:py-16 md:px-32 ">
-            <section className="h-full flex flex-col items-center justify-center border  border-gray-200 bg-white">
+        <div className="items-center  grid grid-cols-[1fr]  bg-[url('/pattern.svg')] grid-rows-1 min-h-dvh py-16 px-32 ">
+            <section className="h-full flex flex-col items-center justify-center   border-gray-200 bg-white">
                 <div className=" w-full flex mb-16 justify-center">
                     <Logo />
                 </div>
