77b89c5ea8a76dbeab185798a04be3e39e75ac63
JamesWithLeo <ocampojamesleo04@gmail.com>
2026-04-02 10:14:12 +0800

    fix deps v1.7

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -18,7 +18,6 @@ import { createHash, randomInt } from "crypto";
 import { and, eq, ne, not } from "drizzle-orm";
 import { checkAuthLimit } from "./actions/rateLimit";
 import { CredentialsSignin } from "next-auth";
-import { compileMjmlTemplate } from "./lib/compileMjmlTemplate";
 
 class RateError extends CredentialsSignin {
     code = "RATE_LIMIT_EXCEEDED";
@@ -72,16 +71,12 @@ export const authConfig = {
                 theme,
                 token,
             }) => {
-                const emailHtml = compileMjmlTemplate("otp", {
-                    name: email.split("@")[0],
-                    otp: token,
-                });
-                // const emailHtml = await render(
-                //     MagicLinkEmail({
-                //         name: email.split("@")[0],
-                //         otp: token,
-                //     })
-                // );
+                const emailHtml = await render(
+                    MagicLinkEmail({
+                        name: email.split("@")[0],
+                        otp: token,
+                    })
+                );
 
                 const response = await fetch("https://api.resend.com/emails", {
                     method: "POST",
